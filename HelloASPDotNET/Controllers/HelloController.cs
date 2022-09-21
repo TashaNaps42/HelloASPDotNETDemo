1217b55f77728de04a0d5cb4e3a4518d25dde75e
Tasha L <natasha.lewis.lalor@gmail.com>
2022-09-21 15:20:26 -0500

    Finished

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,19 +18,54 @@ namespace HelloASPDotNET.Controllers
         {
             string html = "<form method='post' action='/helloworld'>" +
                 "<input type='text' name='name' />" +
+                "<select name='lang' id='lang'>" +
+                "   <option value=''>--Choose a language--</option>" +
+                "   <option value='english'>English</option>" +
+                "   <option value='french'>Francois</option>" +
+                "   <option value='portugese'>Portugese</option>" +
+                "   <option value='chinese'>Chinese</option>" +
+                "   <option value='spanish'>Spanish</option>" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
             return Content(html, "text/html");
         }
 
+        public static string CreateMessage(string name, string lang)
+        {
+            string helloTranslation = "";
+            switch (lang)
+            {
+                case "french":
+                    helloTranslation = "Bonjour";
+                    break;
+                case "english":
+                    helloTranslation = "Hello";
+                    break;
+                case "portugese":
+                    helloTranslation = "Ola";
+                    break;
+                case "chinese":
+                    helloTranslation = "Nihao";
+                    break;
+                case "spanish":
+                    helloTranslation = "Hola";
+                    break;
+                default:
+                    name = "STUPID";
+                    helloTranslation = "Nice job";
+                    break;
+            }
+            return $"<h1>{helloTranslation}, {name}!</h1>";
+        }
+
         // GET: /<controller>/welcome?name=value or GET: /<controller>/welcome/name
         // POST: /<controller>/welcome
         [HttpGet("welcome/{name?}")]
         [HttpPost]
-        public IActionResult Welcome(string name = "World")
+        public IActionResult Welcome(string name = "World", string lang = "english")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            return Content(CreateMessage(name, lang) + "<h3>Return to <a href='https://1.bp.blogspot.com/-MV9UMQk-BdM/U_Ledrs4IYI/AAAAAAAABAQ/AY4uldzgIh4/s1600/1901281_10203148410166011_9139677304055698594_n.jpg'>Hellworld</a>. Wait I mean <a href='/../helloworld'>helloworld</a></h3>", "text/html");
         }
     }
 }
