4a8eb4b660e5e7274af430a2c5c415e44dade3d5
Tasha L <natasha.lewis.lalor@gmail.com>
2022-09-21 13:43:54 -0500

    Start of Tasha's exercise

---
+++ HelloASPDotNET/Controllers/TashasController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class TashasController1 : Controller
+    {
+        [HttpGet]
+        [Route("/tasha/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
